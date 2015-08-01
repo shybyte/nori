9aecea6c55ce58c3f21779802110d85d62f7baca
shybyte <ein@volloeko.de>
2015-08-01 10:25:42 +0200

    Play whole song with one chord per Line

--- Nori.elm
+++ Nori.elm
@@ -11,20 +11,136 @@ import String
 import Task exposing (..)
 import Time
 import Random
+import Random.Array exposing (shuffle)
 import Array
 
 ------------- MODEL ------------
 
 type alias Model = {
-  tickCount: Int,
   text: String,
-  baseNote: Int,
-  currentWord: Maybe SingingWord,
-  midiCommand: Maybe MidiCommand,
-  phonemes: List (List String),
-  phonemesToPlay: List (List String),
-  randomSeed: Random.Seed
+  seed: String,
+
+  tickCount: Int,
+  track: Track,
+  midiCommands: List MidiCommand,
+  phonemes: PhonemeLyrics
 }
+
+
+type alias Track = List Channel
+
+type alias Channel = {
+  midiChannel: Int,
+  notes: Array.Array (List Note)
+}
+
+type alias Note = {
+  pitch: MidiNote,
+  phonemes: PhonemeWord
+}
+
+type alias MidiNote = Int
+
+
+generateTrack : PhonemeLyrics -> Int -> Track
+generateTrack phonemeLyrics seed =
+  let
+    lineNumber = List.length phonemeLyrics
+    randomSeed = Random.initialSeed seed
+    intGenerator = Random.int 0 12
+
+    (chords, seed2) = randomChords randomSeed
+
+
+    pianoChannel = {
+      midiChannel = 0,
+      notes =
+        [0..lineNumber-1]
+        |> List.map
+          (\ln ->
+            generateLine (38 + (Array.get (ln % 4) chords |> Maybe.withDefault 0)) 16
+            |> List.map simpleNote
+          )
+        |> List.concat
+        |> Array.fromList
+    }
+
+    voiceChannel : Channel
+    voiceChannel = {
+      midiChannel = 16,
+      notes =
+        mapRandom generateVoiceLine seed2 phonemeLyrics
+        |> snd
+        |> List.concat
+        |> Array.fromList
+    }
+  in
+    [pianoChannel, voiceChannel]
+
+majorScaleChords = Array.fromList [0, 2, 4, 5, 7, 8]
+
+randomChords: Random.Seed -> (Array.Array Int, Random.Seed)
+randomChords seed =
+  let
+    (shuffledArray, seed') = shuffle seed majorScaleChords
+  in
+    (Array.slice 0 4 shuffledArray, seed')
+
+
+mapRandom : (Random.Seed -> a -> (Random.Seed, b)) -> Random.Seed -> List a -> (Random.Seed, List b)
+--mapRandom : (a -> b) -> List a -> List b
+--mapRandom f randomSeed aList = List.foldr (\aEl list -> f aEl :: [] ) [] aList
+mapRandom f randomSeed aList =
+  let
+    initState = (randomSeed, [])
+    foldF el state =
+      let
+        (randomSeed, bList) = state
+        (randomSeed', b) = f randomSeed el
+      in
+        (randomSeed', b :: bList)
+
+  in
+    List.foldr foldF initState aList
+
+
+
+
+generateVoiceLine : Random.Seed ->  PhonemeLine -> (Random.Seed, List (List Note))
+generateVoiceLine randomSeed phonemeLine =
+  let
+    intGenerator = Random.int 0 8
+    listIntGenerator = Random.list 4 intGenerator
+    (randomList, randomSeed') = Random.generate listIntGenerator randomSeed
+    phonemeLineArray = Array.fromList phonemeLine
+  in
+    List.map2
+      ( \measure randomInt ->
+        [
+          simpleVoiceNote (scalePosToMidi randomInt simpleScale + 50) (Array.get measure phonemeLineArray |> Maybe.withDefault []),
+          [],
+          [],
+          []
+        ]
+      )
+      [0..4-1]
+      randomList
+    |> List.concat
+    |> (\result -> (randomSeed', result))
+
+generateLine : MidiNote -> Int -> List MidiNote
+generateLine baseNote length =
+  List.repeat (length // Array.length pattern) (Array.toList pattern)
+  |> List.concat
+  |> List.map ((+) baseNote)
+
+simpleNote midiNote = [{pitch= midiNote, phonemes=[]}]
+simpleVoiceNote midiNote phonemes= [{pitch= midiNote, phonemes=phonemes}]
+
+type alias PhonemeLyrics = List PhonemeLine
+type alias PhonemeLine = List PhonemeWord
+type alias PhonemeWord = List Phoneme
+type alias Phoneme = String
 
 type alias SingingWord = {
   text: String,
@@ -32,20 +148,24 @@ type alias SingingWord = {
 }
 
 type alias MidiCommand = {
-  note: Int
+  channel: Int,
+  note: Int,
+  phonemes: String
 }
 
 initialModel : Model
 initialModel =
   {
     tickCount = 0,
-    text = "I love you and you love me too",
-    baseNote = 50,
-    currentWord = Nothing,
-    midiCommand = Nothing,
-    phonemes = [],
-    phonemesToPlay = [],
-    randomSeed = Random.initialSeed 123
+    text = """Wise men say
+fools rush in
+you will stay
+joy and sin
+""",
+    seed = "123",
+    track = [],
+    midiCommands = [],
+    phonemes = []
   }
 
 simpleScale = Array.fromList [0, 2, 4, 7, 9]
@@ -71,86 +191,93 @@ type Action =
   NoOp |
   Tick |
   Speak |
-  SetPhonemes (List (List String)) |
-  SetText String
+  SetPhonemes PhonemeLyrics |
+  SetText String |
+  SetSeed String
 
 update : Action -> Model -> Model
 update action model =
   case action of
     SetText text -> {model | text <- text}
-    SetPhonemes phonemes -> {model | phonemes <- phonemes}
+    SetSeed seed -> {model | seed <- seed}
+    SetPhonemes phonemes -> { model |
+      phonemes <- phonemes
+    }
     Tick -> tick model
-    Speak -> {model | phonemesToPlay <- model.phonemes, tickCount <- 0}
+    Speak -> {model |
+      track <- Debug.log "Track: " (generateTrack model.phonemes (parseInt 24 model.seed)),
+      tickCount <- 0
+    }
     otherwise -> model
 
 
 tick : Model -> Model
 tick model =
   let
-    (randomInt,randomSeed') =
-      Random.generate (Random.int 0 5) model.randomSeed
-
-    midiNote =
-      scalePosToMidi randomInt simpleScale + 50
-
-    baseNote' =
-        if model.tickCount % 8 == 0 then
-          midiNote
-        else
-          model.baseNote
-
-    newWord =
-      if model.tickCount % 4 == 0 then
-        case model.phonemesToPlay of
-          [] -> (Nothing)
-          list -> Just { text = nextWord list, freq = midiToFreq(midiNote)}
-      else
-        Nothing
-
-    phonemesToPlay' =
-          if model.tickCount % 4 == 0 then
-            Maybe.withDefault [] (List.tail model.phonemesToPlay)
-          else
-            model.phonemesToPlay
-
-    newMidiCommand =
-      if List.isEmpty model.phonemesToPlay then
-        Nothing
-      else
-        Just {note = baseNote' - 12 + (Array.get (model.tickCount % 4) pattern |> Maybe.withDefault 0 )}
+    newMidiCommands: List MidiCommand
+    newMidiCommands =
+      midiCommandsFromTrackPosition model.track model.tickCount
 
   in
     { model |
         tickCount <- model.tickCount + 1,
-        baseNote <- baseNote',
-        midiCommand <-  newMidiCommand,
-        currentWord <- newWord,
-        phonemesToPlay <- phonemesToPlay',
-        randomSeed <- randomSeed'
+        midiCommands <-  newMidiCommands
     }
+
+
+midiCommandsFromTrackPosition: Track -> Int -> List MidiCommand
+midiCommandsFromTrackPosition track pos =
+  List.map
+    ( \channel ->
+        channel.notes
+        |> Array.get pos
+        |> Maybe.withDefault []
+        |> List.map (simpleMidiCommand channel.midiChannel)
+    )
+    track
+  |> List.concat
+
+simpleMidiCommand : Int -> Note -> MidiCommand
+simpleMidiCommand midiChannel note =
+  {
+    note = note.pitch,
+    channel = midiChannel,
+    phonemes = String.join "" note.phonemes
+  }
 
 
 ------------- VIEWS ------------
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  let field =
+  let
+      speakButton =
+        button [onClick address Speak] [text "Speak"]
+
+      seedField =
         input
+          [ placeholder "Random Seed"
+          , value model.seed
+          , on "input" targetValue (Signal.message address << SetSeed)
+          ]
+          []
+
+      field =
+        textarea
           [ placeholder "English Words"
           , value model.text
           , on "input" targetValue (Signal.message address << SetText)
-          , class "myStyle"
+          , class "textInput"
+          , rows 5
           ]
           []
 
       resultMessage =
-          div [ class "myStyle" ] [  text (flatListListToString model.phonemes)]
-
-      speakButton =
-        button [onClick address Speak] [text "Speak"]
-
+          div []
+            (model.phonemes |> List.map
+              (\line -> div [ class "myStyle" ] [  text (flatListListToString line)]))
   in
-      div [] [field, resultMessage, speakButton]
+      div [] [seedField, speakButton, field, resultMessage]
 
 
 
@@ -166,7 +293,8 @@ main =
 
 model : Signal Model
 model =
-  Signal.foldp update initialModel (Signal.map (Debug.log "Action")  actions.signal)
+  Signal.foldp update initialModel  actions.signal
+  -- Signal.foldp update initialModel (Signal.map (Debug.log "Action")  actions.signal)
 
 
 ------------- Timer ------------
@@ -178,14 +306,9 @@ port ticksTask =
 
 ------------- Interaction with Nori-Voice (Javascript) ------------
 
-port speakPort : Signal (Maybe SingingWord)
-port speakPort =
-    Signal.map .currentWord model
-    --Signal.sampleOn (Signal.filter isSpeakAction NoOp actions.signal) (Signal.map (.phonemes >> flatListListToString) model)
-
-port midiPort : Signal (Maybe MidiCommand)
+port midiPort : Signal (List MidiCommand)
 port midiPort =
-    Signal.map .midiCommand model
+    Signal.map .midiCommands model
 
 ------------- Http Requests ------------
 
@@ -194,24 +317,27 @@ port requests =
   Signal.map lookupPhonemes (Signal.dropRepeats (Signal.map .text model))
     |> Signal.map (\task -> Task.toResult task `andThen` sendResult)
 
-sendResult : (Result String (List (List String))) -> Task x ()
+sendResult : (Result String (PhonemeLyrics)) -> Task x ()
 sendResult = resultToAction >> Signal.send actions.address
 
-resultToAction : Result.Result String (List (List String)) -> Action
-resultToAction result = SetPhonemes (Maybe.withDefault [["AY"]] (Result.toMaybe result))
+resultToAction : Result.Result String (PhonemeLyrics) -> Action
+resultToAction result = SetPhonemes (Maybe.withDefault [[["AY"]]] (Result.toMaybe result))
 
-lookupPhonemes : String -> Task String (List (List String))
+lookupPhonemes : String -> Task String (PhonemeLyrics)
 lookupPhonemes query =
-  let toUrl =
+  let
+    toUrl =
         if String.length query > 0
-          then succeed ("http://localhost:3000/phonemes/" ++ query)
+          then succeed ("http://localhost:3000/phonemes/" ++ Http.uriEncode query)
           else fail "Give me some english words!"
+    debug = Debug.log "query" query
+
   in
         toUrl `andThen` (mapError (always "Not found :(") << Http.get arpabet)
 
 
-arpabet : Json.Decoder (List (List String))
-arpabet = Json.list (Json.list Json.string)
+arpabet : Json.Decoder (PhonemeLyrics)
+arpabet = Json.list (Json.list (Json.list Json.string))
 
 
 
@@ -219,7 +345,6 @@ arpabet = Json.list (Json.list Json.string)
 
 flatListListToString : List (List String) -> String
 flatListListToString list = String.join " " (List.map (String.join "") list)
-
 
 nextWord : List (List String) -> String
 nextWord phonemes =
@@ -233,5 +358,12 @@ makeUSoundLong s =
   if String.startsWith "U" s then "4" ++ s else s
 
 
-midiToFreq : Int -> Float
-midiToFreq midiNote = (440.0 / 32.0) * (2.0 ^ (((toFloat midiNote) - 9.0) / 12.0))
+parseInt: Int -> String -> Int
+parseInt defaultInt s=
+  String.toInt s
+  |> getResultValue defaultInt
+
+getResultValue: a -> Result x a -> a
+getResultValue defaultValue result=
+  Result.toMaybe result
+  |> Maybe.withDefault defaultValue
--- js/nori.js
+++ js/nori.js
@@ -1,26 +1,31 @@
 var noriVoice;
 
 function startNori() {
+  function midiToFreq(midiNote) {
+    return (440.0 / 32.0) * (Math.pow(2.0, (midiNote - 9.0) / 12.0));
+  }
+
+
   // get an empty <div>
   var div = document.getElementById('app');
 
   // embed our Elm program in that <div>
   var noriUI = Elm.embed(Elm.Nori, div, {});
 
-  noriUI.ports.speakPort.subscribe(function (singingWord) {
-    if (singingWord) {
-      console.log('Singing: ', singingWord);
-      noriVoice.sing(singingWord.text + " ", singingWord.freq);
-    }
-  });
-
-  noriUI.ports.midiPort.subscribe(function (midiCommand) {
-    if (midiCommand) {
+  noriUI.ports.midiPort.subscribe(function (midiCommands) {
+    midiCommands.forEach(function (midiCommand) {
       console.log('Playing: ', midiCommand);
-      var delay = 0.15;
-      MIDI.noteOn(0, midiCommand.note, 20, delay);
-      MIDI.noteOff(0, midiCommand.note, delay + 0.75);
-    }
+      if (midiCommand.channel === 16) {
+        if (midiCommand.phonemes) {
+          console.log('Singing: ', midiCommand.phonemes);
+          noriVoice.sing(midiCommand.phonemes + " ", midiToFreq(midiCommand.note));
+        }
+      } else {
+        var delay = 0.15;
+        MIDI.noteOn(0, midiCommand.note, 20, delay);
+        MIDI.noteOff(0, midiCommand.note, delay + 0.75);
+      }
+    });
   });
 }
 
